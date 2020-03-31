623e273ecbaa51c85a3cf339dc1556918759d0ac
unknown <farleyrodolfo@gmail.com>
2020-03-31 01:07:19 -0300

    first commit

---
+++ specflow-coypu/Features/Google.feature
@@ -0,0 +1,9 @@
+﻿Feature: Google
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Acesso a Pagina do Google
+	Given que acesso aa pagina do google "https://www.google.com.br/"
+	And informo o textoo "Cruzeiro"
---
+++ specflow-coypu/WebDrivers/BrowserSessionManager.cs
@@ -0,0 +1,95 @@
+﻿using Coypu;
+using Coypu.Drivers;
+using NUnit.Framework;
+using System;
+using System.IO;
+using TechTalk.SpecFlow;
+
+namespace specflow_coypu.WebDrivers
+{
+    [Binding]
+    public class BrowserSessionManager
+    {
+
+        private BrowserSession browser;
+
+        private Browser browserName;
+
+        public BrowserSession Browser()
+        {
+            return browser;
+        }
+
+        public BrowserSession GetBrowser()
+        {
+            if (browser != null)
+                return browser;
+
+            var sessionConfiguration = new SessionConfiguration()
+            {
+                Browser = GetBrowserName(),
+                AppHost = "https://specflow.org/", // whatever url you want
+                Timeout = TimeSpan.FromSeconds(2)
+            };
+            browser = new BrowserSession(sessionConfiguration);
+            browser.MaximiseWindow();
+
+            return browser;
+        }
+
+        public string CoverPath()
+        {
+            var outputPath = Environment.CurrentDirectory;
+            return outputPath + "\\images\\";
+
+        }
+
+        public void TakeScreenshot()
+        {
+            var resultId = TestContext.CurrentContext.Test.ID;
+
+            var shortpath = Environment.CurrentDirectory + "\\Screenshots";
+
+            if (!Directory.Exists(shortpath))
+            {
+                Directory.CreateDirectory(shortpath);
+            }
+
+            var screenshots = $"{shortpath}\\{resultId}.png";
+
+            browser.SaveScreenshot(screenshots);
+            TestContext.AddTestAttachment(screenshots);
+        }
+
+
+        public void CloseBrowser()
+        {
+            browser.Dispose();
+        }
+
+        private Browser GetBrowserName()
+        {
+            var browserType = TestContext.Parameters["BROWSER"];
+            switch (browserType)
+            {
+                case "CHROME":
+                    browserName = Coypu.Drivers.Browser.Chrome;
+                    break;
+
+                case "INTERNETEXPLORER":
+                    browserName = Coypu.Drivers.Browser.InternetExplorer;
+                    break;
+
+                case "FIREFOX":
+                    browserName = Coypu.Drivers.Browser.Firefox;
+                    break;
+
+                default:
+                    browserName = Coypu.Drivers.Browser.Chrome;
+                    break;
+            }
+            return browserName;
+        }
+
+    }
+}
